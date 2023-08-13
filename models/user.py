b4daca4802fd4a1550b7230df0f14bcc3965b3f9
EmanZHD <zahidimane1@gmail.com>
2023-08-13 23:12:46 +0100

    :)

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+
+
+from models.base_model import BaseModel
+
+
+
+class User(BaseModel):
+    """ Class that defines properties of User """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Creates new instances of User."""
+        
+        super().__init__(*args, **kwargs)
+
